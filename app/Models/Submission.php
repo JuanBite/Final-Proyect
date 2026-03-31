c0f1f8a6ca576b10888aa42667bb1df136bbc388
sebastiangp2004 <sebastiangp20044@gmail.com>
2026-03-31 15:21:48 -0500

    Pull-S

--- app/Models/Submission.php
+++ app/Models/Submission.php
@@ -3,8 +3,25 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Submission extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'project_id',
+        'file_path',
+        'comments',
+        'submitted_at',
+        'grade',
+        'feedback'
+    ];
+
+    // Relation
+
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
 }
--- app/Models/Task.php
+++ app/Models/Task.php
@@ -3,8 +3,25 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Task extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'project_id',
+        'title',
+        'description',
+        'start_date',
+        'due_date',
+        'status',
+        'assigned_to'
+    ];
+
+    // Relation 
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
 }
