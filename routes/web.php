f4e1a5626e218086b62a88b1f66578b82c98c940
sebas <sebastiangp20044@gmail.com>
2026-04-02 18:16:11 -0500

    Fix: web

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,17 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CenterController;
+use App\Http\Controllers\CohortController;
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\projectHistoryController;
+use App\Http\Controllers\projectMemberController;
+use App\Http\Controllers\RegionController;
+use App\Http\Controllers\SubmissionController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\UserController;
+
+
 
 Route::get('/', function () {
     return view('auth.login');
@@ -13,22 +24,30 @@ Route::middleware('auth')->group(function () {
         return view('dashboard');
     })->name('dashboard');
     Route::get('/projects', function () {
-        return view('projects.index');
+        $projects = App\Models\Project::all();
+        return view('projects.index', compact('projects'));
     });
+
     Route::get('/projects/details', function () {
-        return view('projects.details');
+        $projects = App\Models\Project::all();
+        return view('projects.details', compact('projects'));
     });
     Route::get('/stats', function () {
         return view('stats.index');
     });
     Route::get('/users', function () {
-        return view('users.index');
+        $users = App\Models\User::all();
+        return view('users.index', compact('users'));
     });
     Route::get('/users/detail', function () {
-        return view('users.detail');
+        $users = App\Models\User::all();
+        return view('users.detail', compact('users'));
     });
     Route::get('/regions', function () {
-        return view('regions.index');
+        $regions = App\Models\Region::all();
+        $cohorts = App\Models\Cohort::all();
+        $centers = App\Models\Center::all();
+        return view('regions.index', compact('regions','cohorts','centers'));
     });
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
